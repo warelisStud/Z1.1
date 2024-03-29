56fbbda37629905a891235bd944dbc2eeef56d2d
warelisStud <patrycja.warelis@edu.uekat.pl>
2024-03-29 19:21:12 +0100

    Dodaj pliki projektów.

---
+++ Paczka.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z1._1
+{
+    internal class Paczka
+    {
+        private string rodzajPaczki;
+        int dlugosc, wysokosc, szerokosc;
+        int objetosc;
+        public Paczka(string rodzajPaczki, int dlugosc, int wysokosc, int szerokosc) 
+        {
+            this.rodzajPaczki = rodzajPaczki;
+            this.dlugosc = dlugosc;
+            this.wysokosc = wysokosc;
+            this.szerokosc = szerokosc;
+            objetosc = dlugosc * wysokosc * szerokosc;
+        }
+
+        public override string ToString()
+        {
+            return $"Rodzaj paczki: {rodzajPaczki}, długość: {dlugosc}, wysokosc: {wysokosc}, szerokosc: {szerokosc}, objetosc: {objetosc}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Z1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Paczka paczka = new Paczka("ekonomiczna", 2,5,10);
+            Console.WriteLine(paczka.ToString());
+        }
+    }
+}
